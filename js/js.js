0146f6353c6bb29292e6c52a5935da3ca283b449
ivan sergio lopez <ivanlopez_88@hotmail.com>
2018-03-06 15:39:56 -0300

    ok

--- js/js.js
+++ js/js.js
@@ -24,22 +24,18 @@ $(function(){
 
 
         if(nombre==""){
-     
             $("#mensajes_error_email").fadeIn(2000).html("<h2>Debes ingresar un Nombre.</h2");
             return;
         }
         if(!isNaN(nombre)){
-         
             $("#mensajes_error_email").fadeIn(2000).html("<h2>Debes ingresar un Nombre.</h2");
             return;
         }
         if(apellido==""){
-          
             $("#mensajes_error_email").fadeIn(2000).html("<h2>Debes ingresar un Apellido.</h2");
             return;
         }
         if(!isNaN(apellido)){
-        
             $("#mensajes_error_email").fadeIn(2000).html("<h2>Debes ingresar un Apellido.</h2");
             return;
         }
